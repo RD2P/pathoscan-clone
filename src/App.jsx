702759ff92a1532ba02494562dbf3ce16e107a98
Raphael <graphael.dr@gmail.com>
2023-11-22 23:41:20 -0600

    PathoScan hero section demo bootstrap react and vite

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+
+    // Hero
+    <div className="container-fluid hero">
+
+      {/* Logo */}
+      <div className="vspacer-100"></div>
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <img src='./src/assets/logo-whitebg.png' width={200} />
+        </div>
+      </div>
+
+      <div className="vspacer-200"></div>
+
+      {/* Main hero content */}
+      <div className="row px-lg-5">
+
+        {/* Text and buttons */}
+        <div className='col-md-7 mb-4 mb-md-0 d-flex align-items-center px-5'>
+          <div className=''>
+            <h1 className='text-md-start hero-heading'>A revolutionary crop disease diagnostic tool</h1>
+            <hr className='white-hr' />
+            <button className="btn btn-lg btn-patho me-2">GET IN TOUCH</button>
+            <button className="btn btn-lg btn-patho">SIGN UP TODAY</button>
+          </div>
+        </div>
+
+        {/* product picture */}
+        <div className='col-md-5 row justify-content-center align-items-center'>
+          <div className="col-7 col-sm-5">
+            <img className='img-fluid' src='src/assets/pathoscan_product.f56f4a74a5c856df7115.png' ></img>
+          </div>
+        </div>
+      </div>
+    </div >
+
+  )
+}
+
+export default App
---
+++ src/content.jsx
@@ -0,0 +1,38 @@
+<div className="container-fluid p-0 homeGuestContainer">
+  <section className="d-flex heroSection p-0 flex-column">
+    <div className="container-fluid p-0 pt-3">
+      <div className="row col-lg-12 p-0">
+        <div className="col-lg-2 pt-5">
+          <img src="/src/assets/logo-whitebg.png" width="200" />
+        </div>
+        <div class="row container-fluid">
+          <div class="col-lg-2">
+          </div><div class="col-lg-6 align-self-center">
+            <h1 class="heroSection-heading text-left">A revolutionary crop disease diagnostic tool</h1>
+            <hr class="white-hr" />
+            <div class="d-flex col-lg-12 justify-content-center">
+              <div class="col-lg-3">
+              </div>
+              <div class="col-lg-3">
+                <button class="btn btn-patho btn-lg">Get in touch</button>
+              </div>
+              <div class="col-lg-3">
+                <a href="/signup">
+                  <button class="btn btn-patho btn-lg">Sign up today</button>
+                </a>
+              </div>
+              <div class="col-lg-3">
+
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 align-self-center">
+            <img src="/static/media/pathoscan_product.f56f4a74a5c856df7115.png" width="200" />
+          </div>
+          <div class="col-lg-1">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
